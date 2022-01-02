030c5eb9c4cc987e5c04ff19d34b65788a0d5ac3
s22018-pj-Oliwier-Kossak <73356741+s22018-pj-Oliwier-Kossak@users.noreply.github.com>
2022-01-02 18:05:12 +0100

    Update projekt.js

--- projekt.js
+++ projekt.js
@@ -5,3 +5,15 @@ hamburger.addEventListener('click', ()=>{
     navMenu.classList.toggle('show');
 });
 
+function change(){
+    const images =[
+        'url("burgerStart1.jpg")',
+        'url("burgerStart2.jpg")',
+        'url("burgerStart3.jpg")',
+    ]
+    const body = document.querySelector('body');
+    const BG = images[Math.floor(Math.random()*images.length)];
+    body.style.backgroundImage = BG;
+    setInterval(change,2100);
+}
+change();
